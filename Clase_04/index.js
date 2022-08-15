68065fdd8a914a63977f134667bd39ff3c22346a
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-08-15 19:42:44 -0300

    Add files via upload

---
+++ Clase_04/index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+var datoA = 10; // Número
+var nombre = "Playa";// Cadena de texto
+var activo = true // Boleano
+var persona={
+    edad: 34, // Número
+    deporte: 'Correr'// Cadena de texto
+} // Objeto --> registro de datos --> dependiendo de como lo veas
+
+
+console.log(typeof datoA); //imprime en consola el tipo de dato que es el objeto datoA
+console.log(typeof nombre);
+console.log(typeof activo);
+console.log(typeof persona);
+
+// Operador de tipo de datos typeof se puede utilizar para condiciones, comparaciones.
