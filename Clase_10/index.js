d16c0a536cfde3f0e5f72cbc4d48a6fa3625a918
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-08-28 20:26:30 -0300

    Add files via upload

---
+++ Clase_10/index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// Eventos en javascript --> Evento de carga de documento
+
+// Se utiliza con el objeto window
+
+// load --> avisa cuando se termina de cargar la pagina 
+
+window.addEventListener("load",function (){
+    console.log("el contenido de la ventana se ha cargado")
+}) 
+
+
