6208009de35a46770300a1b4ec23c6a6751a0b7b
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-08-29 21:42:31 -0300

    Add files via upload

---
+++ Clase_13/index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Ventanas emergentes o cuadros de dialogo en javascript --> ventanas de alertas
+
+const video = document.querySelector(".bostonVideo");
+
+video.addEventListener("ended",function(){
+alert("el video ha terminado"); // esta pagina dice: ......
+}) // se usa en caso de avisos importantes
+
+// alert compatible con los saltos de linea 
