1bf0bcf5b091487d778e4c7cc5c4148741e73a8f
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-09-04 17:24:52 -0300

    Add files via upload

---
+++ Clase_18/index.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Medir y acceder a un arreglo en JS
+
+var platillos = ["ceviche", "tacos","pasta"]; 
+
+console.log("hay "+ platillos.length +" platillos en el menu"); // platillos.length --> cuenta los elementos del arreglo
+
+var platillo = platillos[0]; // acceder a un elemento del arreglo, en este caso ceviche
+console.log("el platillo seleccionado es:", platillo);
