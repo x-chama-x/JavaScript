bb7a3e61b1cafb80568b3a36c4810654908ed28c
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-09-04 16:48:12 -0300

    Add files via upload

---
+++ Clase_17/index.js
@@ -0,0 +1,12 @@
+"use strict"
+
+// creando mi primer arreglo en JS
+
+
+var platillos = ["ceviche", "tacos","pasta"]; // adentro del corchete son elementos de un arreglo
+// otro metodo para escribir un array
+var bebidas = new Array("Coca","Jugo","Agua");
+console.log(platillos,bebidas);
+console.log(Array.isArray(platillos));
+
+// identificar un arreglo --> console.log(Array.isArray(platillos)); --> devuelve true si es verdadero
