c16507d6a7b9cf0957d7ba18e490628798c2af6c
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-09-04 18:56:36 -0300

    Add files via upload

---
+++ Clase_20/index.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// Operaciones basicas de un arreglo en JS
+
+var platillos = ["ceviche", "tacos","pasta"]; 
+console.log("antes",platillos);
+platillos.push("Pizza"); // metodo que agrega un elemento al arreglo platillos
+platillos.pop(); // metodo que saca el ultimo elemento del arreglo
+console.log("despues",platillos);
+
+// transformar un arreglo en cadena de texto --> var mensaje = platillos.join();
+// console.log(mensaje)
+
+
