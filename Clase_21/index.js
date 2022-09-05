f3d737e08f84c424d2b069151c900d94e31553f1
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-09-04 21:45:28 -0300

    Add files via upload

---
+++ Clase_21/index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Generacion de arreglos en JS con split()
+
+// split()
+
+var mensaje = "pizza, tacos, empanadas";
+var platillos =  mensaje.split(", "); // toma un parametro de base para separar en elementos un texto y los guarda como un arreglo.
+console.log(platillos);
+
+
