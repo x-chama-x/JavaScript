9393269ddee0a333c0f3312a0b763112c0b3fb54
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-09-07 20:52:22 -0300

    Add files via upload

---
+++ Clase_23/index.js
@@ -0,0 +1,8 @@
+"use strict"
+
+// Generacion de arreglos en JS con Array.of
+
+var platillos = Array.of("Pizza","Tacos","Empanadas"); // convierte un texto en un arreglo de "n" datos
+console.log(platillos);
+
+
