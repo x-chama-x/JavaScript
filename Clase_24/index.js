c339acd05dac1f9791f321ca9e8ff98f8c030372
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2022-09-07 21:42:51 -0300

    Add files via upload

---
+++ Clase_24/index.js
@@ -0,0 +1,12 @@
+"use strict"
+
+// Ordenando un arreglo en JS
+
+var platillos = ["Pizza","Asado","Empanadas"];
+console.log("antes ",platillos);
+platillos.sort(); // ordena al arreglo de forma alfabetica
+console.log("ordenado ",platillos);
+platillos.reverse(); // invierte el orden de los elementos de un arreglo
+console.log("despues ",platillos);
+
+
